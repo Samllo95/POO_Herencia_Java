27a2c2de85241a19fa5dbc8e5903e5c184dc88c5
samll <samll@DESKTOP-O513SML>
2020-03-18 22:30:09 -0600

    //POO

--- src/Ejercicio1/Ejercicio1.java
+++ src/Ejercicio1/Ejercicio1.java
@@ -3,6 +3,6 @@ package Ejercicio1;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Ejercicio1.Ejercicio1.main()");
+        System.out.println("Hola mundo");
     }
 }
